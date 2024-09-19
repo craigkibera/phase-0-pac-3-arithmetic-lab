81a611a00d091cc3e8ed78da9461c5a09ff2e00a
craigkibera <craigituro@gmail.com>
2024-09-19 17:48:08 +0300

    Arthmetic

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(){
+    return "something"; 
+}
+function add(a, b) {
+    return a + b;
+}
+
+  function subtract() {
+}
+function divide() {
+}
+function subtract(a,b) {
+    return a-b;
+}
+function multiply(a,b) {
+    return a*b;
+}
+function divide(a,b) {
+    return a/b;
+}
+function increment(n) {
+    return n+1;
+}
+function decrement(n) {
+    return n-1;
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+
